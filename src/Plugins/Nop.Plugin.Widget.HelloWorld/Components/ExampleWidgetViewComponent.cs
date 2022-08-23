3ffaf1df9274cfe2bc8697a31de6af7350d613c4
Shadman Arko <35429182+shadmanArko@users.noreply.github.com>
2022-08-23 20:32:55 +0600

    Tried to build a plugin

---
+++ src/Plugins/Nop.Plugin.Widget.HelloWorld/Components/ExampleWidgetViewComponent.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Nop.Web.Framework.Components;
+
+namespace Nop.Plugin.Widget.HelloWorld.Components
+{
+    [ViewComponent(Name = "HelloWorldWidget")]
+    public class ExampleWidgetViewComponent : NopViewComponent
+    {
+        public IViewComponentResult Invoke(string widgetZone, object additionalData)
+        {
+            return Content("Hello World");
+        }
+    }
+}
---
+++ src/Plugins/Nop.Plugin.Widget.HelloWorld/HelloWorldPlugin.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nop.Services.Cms;
+using Nop.Services.Plugins;
+
+namespace Nop.Plugin.Widget.HelloWorld
+{
+    public class HelloWorldPlugin : BasePlugin,IPlugin, IWidgetPlugin
+    {
+        public bool HideInWidgetList => false;
+
+        public string GetWidgetViewComponentName(string widgetZone)
+        {
+            return "HelloWorldWidget";
+        }
+
+        public Task<IList<string>> GetWidgetZonesAsync()
+        {
+            return Task.FromResult<IList<string>>(new List<string> { "home_page_before_categories" });
+        }
+
+        public override async Task InstallAsync()
+        {
+            //Logic during installation goes here...
+
+            await base.InstallAsync();
+        }
+
+        public override async Task UninstallAsync()
+        {
+            //Logic during uninstallation goes here...
+
+            await base.UninstallAsync();
+        }
+    }
+}
